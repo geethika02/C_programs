97e477c1c8e12505b4312af814a83ce287107838
Geethika Priya <75033631+geethika02@users.noreply.github.com>
2021-04-10 13:21:00 +0530

    Create input.c

---
+++ input.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include<math.h>
+
+int main(){
+	int n,sum=0,a[1000],i=0;
+	scanf("%d",&n);
+	while(n!=-1) 
+	{
+sum=sum+n;
+a[i]=ceil((float)sum/++i);
+printf("%d\n",a[i]);
+
+
+
+
+
+		scanf("%d",&n);
+	
+
+	}
+}           	
